da8c60af59c77229716a0bdb049ddabea47d5e03
Damian Reeves <dreeves@jennison.com>
2015-02-20 16:38:45 -0500

    Flipping bits

---
+++ HackerRankChallengesInFsharp/FlippingBits.fs
@@ -0,0 +1,9 @@
+﻿module FlippingBits
+    open System
+    let run argv = 
+        let lines = Console.ReadLine() |> int
+        for i in 1.. lines do
+            let input = Console.ReadLine() |> uint32
+            printfn "%d" ~~~input
+
+        0 // return an integer exit code
